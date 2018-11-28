4ea00ae9147da16d09a63eb5f8e313562bd6e134
Alan Bignon <alan.bignon@imie.fr>
2018-11-28 16:47:30 +0100

    ajout de ajout d'operation

--- operations.php
+++ operations.php
@@ -17,6 +17,23 @@
         <th>montant</th>
     </tr>
     </thead>
+    <?php
+    //connection au serveur
+
+    $bdd = new PDO('mysql:host=localhost;dbname=compte_bancaire;charset=utf8', 'root', '');
+
+    //récupération des valeurs des champs:
+    //label:
+    $label     = $_POST["label"] ;
+    //montant:
+    $montant     = $_POST["montant"] ;
+
+    //création de la requête SQL:
+    $bdd ->exec( "INSERT  INTO test (label, montant)VALUES ( '$label', '$montant' ) ") ;
+
+    echo "ajouté";
+
+    ?>
 </table>
 </body>
 </html>
