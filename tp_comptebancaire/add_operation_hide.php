5a769e8f95c2d2ae431a4c4a2943f8d9b855633c
Alan Bignon <alan.bignon@imie.fr>
2018-11-29 12:39:25 +0100

    tout nickel

---
+++ tp_comptebancaire/add_operation_hide.php
@@ -0,0 +1,25 @@
+<?php
+
+    //connection au serveur
+
+    $bdd = new PDO('mysql:host=localhost;dbname=compte_bancaire;charset=utf8', 'root', '');
+
+    //récupération des valeurs des champs:
+
+    //label:
+    $libelle     = $_POST["libelle"] ;
+    //montant:
+    $montant     = $_POST["montant"] ;
+
+
+    //création de la requête SQL:
+
+    //recuperation du nombre de transaction deja présente
+    //$SELECT_COUNT = "SELECT COUNT(*) FROM transaction";
+    //$result = $bdd->query($SELECT_COUNT);
+    //$count = $result->fetch();
+    //creation de la nouvelle ligne
+    $bdd ->exec("INSERT  INTO transaction (libelle, montant)VALUES ('$libelle', '$montant') ") ;
+
+    header('location: http://localhost/tp_phpobjet/operations.php')
+    ?>
